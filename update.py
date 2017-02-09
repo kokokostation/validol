cdd621c54ba31e845441436f5e494f11544cc68d
Aleksandr Manakov <kokokostation@gmail.com>
2017-02-10 00:23:42 +0300

    3

--- update.py
+++ update.py
@@ -1,14 +1,29 @@
 import requests
 import zipfile
 import io
+import os
+import shutil
 
 def update_sources():
     archive = requests.get("https://github.com/kokokostation/COTs/archive/master.zip").content
     file = io.BytesIO(archive)
 
     zipFile = zipfile.ZipFile(file,"r")
-    for name in zipFile.namelist()[1:]:
-        open(name.split("/")[1], "wb").write(zipFile.read(name))
+    zipFile.extractall()
+    path = zipFile.namelist()[0]
+    zipFile.close()
+
+    for target in os.listdir(path):
+        try:
+            shutil.rmtree(target)
+        except NotADirectoryError:
+            try:
+                os.unlink(target)
+            except FileNotFoundError:
+                pass
+        shutil.move(os.path.join(path, target), target)
+
+    shutil.rmtree(path)
 
 if __name__ == '__main__':
     update_sources()
